93e157b253fbed1779ccee808404cff56816d018
Taivo <taivo.nurk@khk.ee>
2020-09-22 09:37:30 +0300

    Kood lühendatud

--- 1/yl4.py
+++ 1/yl4.py
@@ -1,6 +1,6 @@
 nimi = input("Sisestage oma nimi:")
 lubatud = input("Sisestage lubatud kiirus (km/h):")
-tegelik = input("Sisestage teglik kiirus (km/h):")
+tegelik = input("Sisestage tegelik kiirus (km/h):")
 vastus = str(min(190,(int(tegelik) - int(lubatud))* 3))
 
-print(nimi +",kiiruse ületamise eest on teie trahv " + vastus + " eurot.")
+print(nimi +", kiiruse ületamise eest on Teie trahv " + vastus + " eurot.")
