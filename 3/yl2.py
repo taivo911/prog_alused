38f404a34dcdff8be0e38ced6ac643ad97e7c363
Taivo <taivo.nurk@khk.ee>
2020-10-01 15:18:24 +0300

    Ulesanne 3.2 lahendus

--- 3/yl2.py
+++ 3/yl2.py
@@ -6,7 +6,7 @@ ringi_number = 1 # alustame jooksu 1. ringist
 porgandite_arv = 0
 
 while(ringi_number <= ringide_arv):
-    print(str(ringi_number) + ". ring")  #protsessi kontrollida saab printides ringi_number, kuvab 7 tk.
+    print(str(ringi_number) + ". ring")  #protsessi kontrollida saab printides ringi_number, kuvab 7 tk
     #suurendame muutuja v22rtust ringi l2bimisel
     if(ringi_number % 2 == 0):
         print("Said " + str(ringi_number) + " porgandit.")
