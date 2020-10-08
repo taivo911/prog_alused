b0346e861fa5014ba42f740596cd3564c6d1f1eb
Taivo <taivo.nurk@khk.ee>
2020-10-08 09:23:20 +0300

    YL 4.1 lahendus ver2

--- 4/yl1.py
+++ 4/yl1.py
@@ -1,3 +1,7 @@
-print('Sisesta ees- ja perekonnanimi:')
-nimi = str(input()).title()
-print(nimi)
+#print('Sisesta ees- ja perekonnanimi:')
+#nimi = str(input()).title()
+#print(nimi)
+
+nimi = input("Sisest ees ja perekonnanimi: ")
+for osanimest in nimi.split(" "):
+    print(osanimest.capitalize(), end=" ")
